ebbdae8f92fd95a3ba37740c269af46663f94364
LozovskiM <37662663+LozovskiM@users.noreply.github.com>
2020-05-28 12:06:30 +0300

    add movie controller (#7)

---
+++ Cinema system/Cinema system/Controllers/MoviesController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CinemaSystem.Interfaces;
+using CinemaSystem.Models;
+
+namespace CinemaSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private readonly IMovieService _movieService;
+
+        private const string ErrorOfMovieNonexistence = "This movie does not exists.";
+        private const string ErrorOfMovieExistence = "This movie is already exists.";
+        private const string ErrorOfMovieDate = "Ending date must be later than release date";
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            var movies = _movieService.GetMovies();
+
+            return Ok(new GetResponse<IEnumerable<MovieView>>(movies));
+        }
+
+        [HttpGet("{movieId}")]
+        public IActionResult GetMovie(int movieId)
+        {
+            if (!_movieService.CheckMovieExists(movieId))
+            {
+                return NotFound(new Response(ErrorOfMovieNonexistence));
+            }
+
+            var movie = _movieService.GetMovie(movieId);
+
+            return Ok(new GetResponse<MovieFullView>(movie));
+        }
+
+        [HttpPost]
+        public IActionResult CreateMovie([FromBody] MovieFullView movie)
+        {
+            if (_movieService.CheckMovieExists(movie))
+            {
+                return BadRequest(new Response(ErrorOfMovieExistence));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response(ModelState));
+            }
+
+            if (_movieService.CheckMovieDate(movie))
+            {
+                return BadRequest(new Response(ErrorOfMovieDate));
+            }
+
+            int id = _movieService.CreateMovie(movie);
+
+            var response = new CreateResponse(id);
+
+            return CreatedAtAction(nameof(GetMovie), new { id = response.Id }, response);
+        }
+
+        [HttpPut("{movieId}")]
+        public IActionResult EditMovie([FromBody] MovieFullView movie, int movieId)
+        {
+            if (!_movieService.CheckMovieExists(movieId))
+            {
+                return NotFound(new Response(ErrorOfMovieNonexistence));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response(ModelState));
+            }
+
+            if (_movieService.CheckMovieDate(movie))
+            {
+                return BadRequest(new Response(ErrorOfMovieDate));
+            }
+
+            _movieService.EditMovie(movie, movieId);
+
+            return Ok(new Response());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int movieId)
+        {
+            if (!_movieService.CheckMovieExists(movieId))
+            {
+                return NotFound(new Response(ErrorOfMovieNonexistence));
+            }
+
+            _movieService.DeleteMovie(movieId);
+
+            return Ok(new Response());
+        }
+
+    }
+}
---
+++ Cinema system/Cinema system/Models/ViewModels/MovieFullView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaSystem.Models
+{
+    public class MovieFullView
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title cannot be empty")]
+        [StringLength(50, ErrorMessage = "Title must be less than 50 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Release date is required")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Ending date is required")]
+        public DateTime EndingDate { get; set; }
+
+        [Required(ErrorMessage = "Description cannot be empty")]
+        [StringLength(256, ErrorMessage = "Description must be less than 256 characters")]
+        public string Description { get; set; }
+    }
+}
